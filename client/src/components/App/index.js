7f000886d6db80ddbbba329966755101c7487605
fadeomar <fadeomar2015@gmail.com>
2019-10-25 21:30:36 +0300

    install concurently and check the conection between server and client side

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './style.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    x: '',
+  };
+
+  componentDidMount() {
+    this.getdata();
+  }
+
+  getdata = () => {
+    fetch('api/v1')
+      .then(res => res.json())
+      .then(res => this.setState({ x: res }))
+      .catch(console.log);
+  };
+
+  render() {
+    const { x } = this.state;
+    return <div className="App">{x}</div>;
+  }
 }
 
 export default App;
