04482050002b57782c77c66c2b2db46e1b48b74b
fadeomar <fadeomar2015@gmail.com>
2019-11-14 00:30:31 +0200

    add the filter to the common index

--- client/src/components/common/Filter/index.js
+++ client/src/components/common/Filter/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Select, Form, Button, Switch, notification } from 'antd';
 
-import Loader from '../index';
+import Loader from '../Loader';
 import { types, ranges } from './staticData';
 import './style.css';
 
--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,5 +1,8 @@
 import Header from './Header';
 import Button from './button/Button';
 import Loader from './Loader';
+import Card from './Card';
+import Questionnaire from './Questionnaire';
+import FilterComponent from './Filter';
 
-export { Header, Button, Loader };
+export { Header, Button, Loader, Questionnaire, Card, FilterComponent };
