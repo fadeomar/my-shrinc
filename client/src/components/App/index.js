15efaf8a68c8f0dccabbfdcc4d4f7354424e2965
fadeomar <fadeomar2015@gmail.com>
2019-11-14 20:31:24 +0200

    create the About page and addit to the app

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { Header } from '../common';
-import { Home, NotFound, Glossary } from '../pages';
+import { Home, NotFound, Glossary, About } from '../pages';
 
 import './style.css';
 
@@ -16,6 +16,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/glossary" component={Glossary} />
+          <Route path="/About" component={About} />
           <Route path="*" component={NotFound} />
         </Switch>
       </BrowserRouter>
--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,5 +1,6 @@
 import Home from './Home';
 import NotFound from './NotFound';
 import Glossary from './Glossary';
+import About from './About';
 
-export { Home, NotFound, Glossary };
+export { Home, NotFound, Glossary, About };
